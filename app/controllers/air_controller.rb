2282c86bd197f5c0530bab84054213c2fa546267
yihui <yihui.han@kindroid.com>
2013-03-01 16:35:14 +0800

    framework finished

--- app/controllers/air_controller.rb
+++ app/controllers/air_controller.rb
@@ -1,23 +1,29 @@
 class AirController < ApplicationController
   def index  	
   	puts "Log: AirController-->Index1 called with method get"
-  	params.each do |key,value|
-	  puts "Log: Param #{key}: #{value}"
-	end
-
-	request_method=request.method
-	if request_method=='GET'
-		user_sign
-	elsif request_method=='POST'
-		get_msg
-	end
+  	  	
+  	if request.get?
+  		result=user_sign(params)
+      render :text=>result
+  	elsif request.post?
+  		result=get_msg(params)
+      render :text=>result
+  	end
+    return
   end
 
-  def user_sign
+  def user_sign(params)
   	puts "Log: user_sign method called"
+    params.each do |key,value|
+      puts "Log: Param #{key}: #{value}"
+    end
+  	return params['echostr'] if params.has_key?('echostr')
   end
 
-  def get_msg
+  def get_msg(params)
   	puts "Log: get_msg method called"
+    params.each do |key,value|
+      puts "Log: Param #{key}: #{value}"
+    end
   end
 end
